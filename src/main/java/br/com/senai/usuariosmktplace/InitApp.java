b88ea37c53d0573b2a2bab2bda329c1dd6b062de
Murilo Vieira <murilo12super@gmail.com>
2023-08-18 22:10:58 -0300

    Implementado pré-requisitos para desenvolvimento das regras de negócio

--- src/main/java/br/com/senai/usuariosmktplace/InitApp.java
+++ src/main/java/br/com/senai/usuariosmktplace/InitApp.java
@@ -1,8 +1,14 @@
 package br.com.senai.usuariosmktplace;
 
+import br.com.senai.usuariosmktplace.core.service.UsuarioService;
+
 public class InitApp {
 
 	public static void main(String[] args) {
+		UsuarioService service = new UsuarioService();
 		
+		System.out.println(service.removerAcentoDo("José da Sílva"));
+		System.out.println(service.fracionar("Murilo de Dolores da silva"));
+		System.out.println(service.gerarLoginPor("Dick Vigarista"));
 	}
 }
